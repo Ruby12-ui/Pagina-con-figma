fbaa2d7d73efdeda26413fd8055055709f584d13
Ruby Cubas Villanueva <rcubas@vallegrande.edu.pe>
2026-03-25 22:44:54 -0500

    Add files via upload

---
+++ pagina con figma/script.js
@@ -0,0 +1,11 @@
+function comprar() {
+    alert("Producto agregado al carrito");
+}
+
+function enviar() {
+    alert("Mensaje enviado correctamente");
+}
+
+function irProductos() {
+    window.location.href = "productos.html";
+}
